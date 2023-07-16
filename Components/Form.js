72b43004c4487966d0f7b6f60d419933e26e0a53
Shaukat Sohail <65353819+Shaukat456@users.noreply.github.com>
2023-07-16 22:38:53 +0500

    latest

--- Components/Form.js
+++ Components/Form.js
@@ -19,7 +19,8 @@ export const IbanForm = () => {
         <div className="w-full rounded-lg bg-white shadow dark:border dark:border-gray-700 dark:bg-gray-800 sm:max-w-md md:mt-0 xl:p-0">
           <div className="space-y-4 p-6 sm:p-8 md:space-y-6">
             <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 dark:text-white md:text-2xl">
-              Sign in to your account
+            Enter Iban Number
+
             </h1>
             <form className="space-y-4 md:space-y-6">
               <div>
@@ -45,7 +46,7 @@ export const IbanForm = () => {
                 className="bg-primary-600 hover:bg-primary-700 focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800 w-full rounded-lg bg-black px-5 py-2.5 text-center text-sm font-medium text-white focus:outline-none   focus:ring-4 "
                 onClick={handleSubmit}
               >
-                SUBMIt
+                SUBMIT
               </button>
             </form>
           </div>
