64d032e223842948a569c87588de3369d4ddacc0
Shaukat Sohail <65353819+Shaukat456@users.noreply.github.com>
2023-07-16 20:32:06 +0500

    changes

--- Components/Navbar.js
+++ Components/Navbar.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 
 export const Navbar = (props) => {
-  const { user } = props
+  const { user , setUser } = props
   return (
     <div>
       <nav className="mb-10 border-gray-200  dark:bg-gray-900">
@@ -30,12 +30,13 @@ export const Navbar = (props) => {
             <ul className="mt-4 flex flex-col rounded-lg border border-gray-100 bg-gray-50 p-4 font-medium dark:border-gray-700 dark:bg-gray-800 md:mt-0 md:flex-row md:space-x-8 md:border-0 md:bg-white md:p-0 md:dark:bg-gray-900">
 
               <li>
-                <a
+                <button
                   href="#"
                   className="block rounded py-2 pl-3 pr-4 text-gray-900 hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:bg-transparent md:dark:hover:text-blue-500"
+                  onClick={()=>setUser(false)}
                 >
-                  {user ? "Logout" : "Login"}
-                </a>
+                  {user && "Logout" }
+                </button>
               </li>
             </ul>
           </div>
--- pages/Login.js
+++ pages/Login.js
@@ -73,7 +73,6 @@ export const Login = ({setUser}) => {
               <button
                 type="submit"
                 className="bg-primary-600 hover:bg-primary-700 focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800 w-full rounded-lg bg-black px-5 py-2.5 text-center text-sm font-medium text-white focus:outline-none   focus:ring-4 "
-                // onClick={handleSubmit}
               >
                 Sign in
               </button>
